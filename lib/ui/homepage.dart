8221da096d5262d348d4f0a6d4e366ba098c2154
under3415 <3857822+under3415@users.noreply.github.com>
2023-11-25 10:00:32 +1300

    Update homepage.dart
    Adding email verification

--- lib/ui/homepage.dart
+++ lib/ui/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:rewardscard/ui/login.dart';
 import '../service/auth_service.dart';
@@ -9,6 +10,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final AuthService authService = AuthService();
     final nav = Navigator.of(context);
+    final snackbar = ScaffoldMessenger.of(context);
 
     return Scaffold(
       appBar: AppBar(
@@ -22,17 +24,51 @@ class HomePage extends StatelessWidget {
                     Text('Welcome, ${AuthService().getCurrentUser()?.email}')),
           ),
           ElevatedButton(
-              onPressed: () async {
-                await authService.signout();
-                nav.pushReplacement(
-                  MaterialPageRoute(
-                      builder: (_) =>
-                          LoginPage()), // Replace with your next screen
-                );
-              },
-              child: const Text(
-                "Sign Out",
-              ))
+            onPressed: () async {
+              User? user = authService.getCurrentUser();
+              if (user?.emailVerified ?? false) {
+                snackbar.showSnackBar(
+                    const SnackBar(content: Text('Already verified!')));
+                return;
+              }
+
+              await authService.reloadUser();
+              user = authService.getCurrentUser();
+
+              if (user == null) {
+                snackbar.showSnackBar(
+                    const SnackBar(content: Text('User not found!')));
+                return;
+              }
+              if (user.emailVerified) {
+                snackbar.showSnackBar(
+                    const SnackBar(content: Text('You are now verified!')));
+                
+              } else {
+                await authService.sendEmailVerification();
+                snackbar.showSnackBar(const SnackBar(
+                    content: Text(
+                        'Check your email for verification code, then come back here to complete verification process!')));
+              }
+            },
+            child: const Text(
+              "Verify email",
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+              await authService.signout();
+              nav.pushReplacement(
+                MaterialPageRoute(
+                    builder: (_) =>
+                        LoginPage()), // Replace with your next screen
+              );
+            },
+            child: const Text(
+              "Sign Out",
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
         ],
       ),
     );

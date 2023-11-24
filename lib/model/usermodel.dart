720c1191ef9d5c1ef8e2c1d3a9ba3d0d62febd70
under3415 <3857822+under3415@users.noreply.github.com>
2023-11-24 13:24:45 +1300

    Update usermodel.dart
    adding method to allow updates from results

--- lib/model/usermodel.dart
+++ lib/model/usermodel.dart
@@ -1,11 +1,19 @@
+import 'package:rewardscard/model/signinresult.dart';
+
 class UserModel {
-  final String? userId;
-  final String? name;
-  final String? email;
+  String userId;
+  String name;
+  String email;
 
   UserModel({
-    this.userId,
-    this.name,
-    this.email,
+    required this.userId,
+    required this.name,
+    required this.email,
   });
+
+  void updatePropertiesFromSignInResult(SignInResult result) {
+    userId = result.user!.userId;
+    name = result.user!.name;
+    email = result.user!.email;
+  }
 }

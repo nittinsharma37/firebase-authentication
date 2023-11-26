2ef0c8e6eb4738e89432fcd337c0f1c03fba3021
under3415 <3857822+under3415@users.noreply.github.com>
2023-11-26 19:31:53 +1300

    Update usermodel.dart
    adding emailVerified

--- lib/model/usermodel.dart
+++ lib/model/usermodel.dart
@@ -2,12 +2,12 @@ class UserModel {
   String userId;
   String name;
   String email;
+  bool emailVerified;
 
   UserModel({
     required this.userId,
     required this.name,
     required this.email,
+    required this.emailVerified,
   });
-
- 
 }

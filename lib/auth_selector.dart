2e3058b66c11dcb66aaed069c8fe742a8d99110c
under3415 <3857822+under3415@users.noreply.github.com>
2023-11-26 19:35:02 +1300

    Update auth_selector.dart
    going back to using AuthSelector

--- lib/auth_selector.dart
+++ lib/auth_selector.dart
@@ -1,45 +1,74 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'service/auth_service.dart';
-import 'ui/homepage.dart';
-import 'ui/login.dart';
+import 'auth_selector.dart';
 import 'model/usermodel.dart';
+import 'service/auth_service.dart';
 
-class AuthSelector extends StatelessWidget {
-  const AuthSelector({super.key});
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-  Future<void> reloadUser() async {
-    //this is used to reload user
-    //reloading ensures that we have the latest info from firebase
-    //without reload, old data is read from cache
-    print('debug_auth: user reloaded');
-    AuthService authService = AuthService();
-    await authService.reloadUser();
-  }
+  runApp(
+    MyApp(),
+  );
+}
 
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    print('debug_auth: authSelector rebuilt');
-
-    //this widget listens to stream and rebuilds every time there is a change in user (e.g. sign out)
-    //this will navigate to either login or home page
-    final user = Provider.of<UserModel?>(context);
-
-    if (user != null) {
-      return FutureBuilder(
-        future: reloadUser(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Firebase Auth",
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        inputDecorationTheme: const InputDecorationTheme(
+          focusColor: Colors.blue,
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontSize: 42.0, fontWeight: FontWeight.bold),
+          titleLarge: TextStyle(fontSize: 26.0),
+          bodyMedium: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+        colorScheme:
+            ColorScheme.fromSwatch(primarySwatch: Colors.blue).copyWith(
+          secondary: const Color(0xffBFFF80),
+        ),
+      ),
+      home: FutureBuilder(
+        future: _initialization,
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator(); // Display a loading spinner while fetching data
-          } else if (snapshot.hasError) {
-            return LoginPage();
-          } else {
-            return const HomePage();
+          if (snapshot.hasError) {
+            return const Scaffold(
+              body: Center(
+                child: Text("Something went wrong!"),
+              ),
+            );
           }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MultiProvider(
+              providers: [
+                //stream provider listens to changes in firebase user
+                //filter for "debug_auth" in debug console to see key events
+                StreamProvider<UserModel?>.value(
+                  initialData: null,
+                  catchError: (_, op) => null,
+                  //updateShouldNotify: (_, __) => true,
+                  value: AuthService().user,
+                ),
+              ],
+              child:
+                  const AuthSelector(), //select either login or home page based on user state
+            );
+          }
+          return const Scaffold(
+              body: Center(
+            child: CircularProgressIndicator(),
+          ));
         },
-      );
-    } else {
-      return LoginPage();
-    }
+      ),
+    );
   }
 }

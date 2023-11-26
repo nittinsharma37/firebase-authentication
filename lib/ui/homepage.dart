7a1a15d97c03d7388c1c07699f284f1be2e3b2b9
under3415 <3857822+under3415@users.noreply.github.com>
2023-11-26 19:36:37 +1300

    Update homepage.dart
    using provider

--- lib/ui/homepage.dart
+++ lib/ui/homepage.dart
@@ -1,16 +1,20 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'login.dart';
+import 'package:provider/provider.dart';
+import '../model/signinresult.dart';
+import '../model/usermodel.dart';
 import '../service/auth_service.dart';
+import 'shared.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    print('debug_auth: HomePage rebuilt');
+
     final AuthService authService = AuthService();
-    final nav = Navigator.of(context);
     final snackbar = ScaffoldMessenger.of(context);
+    final userProvider = Provider.of<UserModel?>(context);
 
     return Scaffold(
       appBar: AppBar(
@@ -20,25 +24,30 @@ class HomePage extends StatelessWidget {
         children: [
           Expanded(
             child: Center(
-                child:
-                    Text('Welcome, ${AuthService().getCurrentUser()?.email}')),
+              child: Text(
+                  'Welcome ${userProvider?.email ?? 'unknown'} (${userProvider?.emailVerified ?? false ? 'verified' : 'unverified'})'),
+            ),
           ),
-          ElevatedButton(
-            onPressed: () async {
-              await verifyEmail(authService, snackbar);
-            },
-            child: const Text(
-              "Verify email",
+          Visibility(
+            visible: !(userProvider?.emailVerified ??
+                false), //hide verify email button if the user is verified already
+            child: ElevatedButton(
+              onPressed: () async {
+                await verifyEmail(authService, snackbar,
+                    userProvider); //send verification email
+              },
+              child: const Text(
+                "Verify email",
+              ),
             ),
           ),
           TextButton(
             onPressed: () async {
-              await authService.signout();
-              nav.pushReplacement(
-                MaterialPageRoute(
-                    builder: (_) =>
-                        LoginPage()), // Replace with your next screen
-              );
+              SignInResult? result = await authService.signout();
+              snackbarHandler(result, snackbar,
+                  'Check your email for verification code, then come back here to complete verification process!');
+
+              //do not navigate from here, user change will cause AuthSelector rebuild which will return the correct page
             },
             child: const Text(
               "Sign Out",
@@ -50,41 +59,24 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Future<void> verifyEmail(
-      AuthService authService, ScaffoldMessengerState snackbar) async {
-    //get user from service
-    User? user = authService.getCurrentUser();
-
+  Future<void> verifyEmail(AuthService authService,
+      ScaffoldMessengerState snackbar, UserModel? user) async {
     //check if user is already verified and exit early if true
     if (user?.emailVerified ?? false) {
       snackbar.showSnackBar(const SnackBar(content: Text('Already verified!')));
       return;
     }
 
-    //reload user. This is necessary as the cached user isn't refreshed and the verifyEmail value never changes
-    await authService.reloadUser();
-    //get the reloaded user
-    user = authService.getCurrentUser();
+    //send verification email
+    SignInResult result = await authService.sendEmailVerification();
 
-    //check if the user is null. This is not expected but could happen if the user is deleted since being verified
-    if (user == null) {
-      snackbar.showSnackBar(const SnackBar(content: Text('User not found!')));
-      return;
-    }
+    snackbarHandler(result, snackbar,
+        'Check your email (including spam folder) for verification link, then come back here to complete verification process!');
 
-    //if the user is verified after reload, display that and exit, otherwise initiate verification
-    //one downside here is that user could spam the mail by clicking this without going to her email to actually verify
-    //perhaps we should limit this to once every 3 minutes or something?
-    if (user.emailVerified) {
-      snackbar
-          .showSnackBar(const SnackBar(content: Text('You are now verified!')));
-    } else {
-      //send verification email
-      await authService.sendEmailVerification();
-      snackbar.showSnackBar(const SnackBar(
-          content: Text(
-              'Check your email for verification code, then come back here to complete verification process!')));
-    }
+    result = await authService.signout();
+
+    //display errors if any
+    snackbarHandler(result, snackbar, '');
     return;
   }
 }
